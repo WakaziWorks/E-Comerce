6605c13e56262adff85071abb974df9a295f1226
AbiollaghJames <abiollajames@protonmail.com>
2024-03-07 08:51:28 +0300

    cart page

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,14 +3,14 @@
         <div class="container">
             <div class="row">
                 <div class="col">
-                    <h3 style="text-align: left; color: purple;">Shopping Guide</h3>
+                    <h3 style="text-align: left; color: white;">Shopping Guide</h3>
                     <a href="#"><p style="text-align: left;">How do I pay on wakazi?</p></a>
                     <a href="#"><p style="text-align: left;">How long does my order arrive?</p></a>
                     <a href="#"><p style="text-align: left;">How to shop on wakazi?</p></a>
                     <a href="#"><p style="text-align: left;">Forgot password?</p></a>
                 </div>
                 <div class="col">
-                    <h3 style="text-align: left; color: purple;">Customer Help</h3>
+                    <h3 style="text-align: left; color: white;">Customer Help</h3>
                     <a href="#"><p style="text-align: left;">Privacy Policy</p></a>
                     <a href="#"><p style="text-align: left;">Terms and Conditions</p></a>
                     <a href="#"><p style="text-align: left;">Account Settings</p></a>
@@ -18,17 +18,17 @@
                     <a href="#"><p style="text-align: left;">FAQ Center</p></a>
                 </div>
                 <div class="col">
-                    <h3 style="text-align: left; color: purple;">Business</h3>
+                    <h3 style="text-align: left; color: white;">Business</h3>
                     <a href="#"><p style="text-align: left;">Want to be a Seller?</p></a>
                     <a href="#"><p style="text-align: left;">Seller Center</p></a>
                     <a href="#"><p style="text-align: left;">Wakazi Shop</p></a>
                 </div>
                 <div class="col">
-                    <h3 style="text-align: left; color: purple;">Stay Connected</h3>
-                    <a href="#"><i class="fi fi-brands-facebook"></i></a>
-                    <a href="#"><p style="text-align: left;">How long does my order arrive?</p></a>
-                    <a href="#"><p style="text-align: left;">How to shop on wakazi?</p></a>
-                    <a href="#"><p style="text-align: left;">Forgot password?</p></a>
+                    <h3 style="align-items: start; color: white;">Stay Connected</h3>
+                    <a href="https://www.youtube.com/@WakaziWorks" target="blank"><i class="bi bi-youtube"></i></a>
+                    <a href="https://ke.linkedin.com/company/wakazi-works-platform" target="blank"><i class="bi bi-linkedin"></i></a>
+                    <a href="https://twitter.com/wakaziworks" target="blank"><i class="bi bi-twitter"></i></a>
+                    <a href="https://www.instagram.com/wakazi_works/" target="blank"><i class="bi bi-instagram"></i></a>
                 </div>
             </div>
             </div>
