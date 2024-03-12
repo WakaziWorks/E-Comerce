f526adada35a0c9d6f045ad4665071a9ee63ade2
skeeperloyaltie <gugod254@gmail.com>
2024-03-12 15:09:06 +0300

    changes

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -58,8 +58,19 @@
                                     <h4>Welcome Guest</h4>
                                     <p>Manage Cart, Orders & Wishlist</p>
                                     <hr />
-                                    <button class="signin-btn">Sign In</button>
-                                    <button class="signup-btn">Sign Up</button>
+                                    @if (Route::has('login'))
+                <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
+                    @auth
+                        <a href="{{ url('/home') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Home</a>
+                    @else
+                        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
                                 </div>
                             </div>
                         </div>
