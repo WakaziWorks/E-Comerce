d21fd0cbf39162a1738c5f038f867ee8417a35f1
AbiollaghJames <abiollajames@protonmail.com>
2024-03-25 02:25:06 +0300

    nav fixes

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,4 +1,4 @@
-    <div class="header fixed-top">
+    <header class="header fixed-top">
         <div class="nav">
             
             <div class="f-nav">
@@ -18,21 +18,11 @@
                         <a href="#">Books, Movies, Music</a>
                     </div>
                 </div>
-                <a class="navbar-brand fw-bold" id="logo" href="{{ route('home') }}">
-                    <img src="{{ asset('images/WhatsApp_Image_2024-02-28_at_15.48.15-removebg-preview.png') }}" height="90px" width="110px">
-                </a>
-                <div></div>
-                <form class="search-form" role="search">
-                    <div class="input-group">
-                        <input class="form-control me-2" type="search" placeholder="I am looking for..." aria-label="Search">
-                        <button class="btn btn-outline-success" type="submit">Search</button>
-                    </div>
-                </form>
-                <div class="icons">
-                    <a href="{{ route('cart') }}" style="margin: 10px; text-decoration: none;">
-                        <img src="{{ asset('images/cart4.svg') }}" />
-                        <span>Cart</span>
+                <div class="search-section">
+                    <a class="navbar-brand fw-bold" id="logo" href="{{ route('home') }}">
+                        <img src="{{ asset('images/WhatsApp_Image_2024-02-28_at_15.48.15-removebg-preview.png') }}" height="90px" width="110px">
                     </a>
+<<<<<<< HEAD
                     <a href="" style="margin: 10px; text-decoration: none;" id="popup-trigger">
                         <img src="{{ asset('images/person-check.svg') }}" />
                         <span>Account</span>
@@ -60,19 +50,77 @@
                         </div>
                         @endif
                     </div>
+=======
+>>>>>>> 9fe34890 (nav fixes)
                 </div>
             </div>
-            <nav class="f-nav-two" style="margin-top: 0;">
-                <ul class="row-btn">
-                    <li><a href="{{ route('home') }}"><span>Home</span></a></li>
-                    <li><a href="{{ route('products') }}"><span>Products</span></a></li>
-                    <li><a href="{{ route('collections') }}"><span>Collections</span></a></li>
-                    <li><a href="{{ route('blog') }}"><span>Blog</span></a></li>
-                    <li><a href="{{ route('features') }}"><span>Features</span></a></li>
-                </ul>
-            </nav>
+            <div class="menu-toggle1" onclick="toggleNavs()">
+                <div class="dash"></div>
+                <div class="dash"></div>
+                <div class="dash"></div>
+            </div>
+            <!-- <div class="nav-container"> -->
+                <div class="f-nav-two">
+                    <form class="search-form" role="search">
+                        <div class="input-group">
+                            <input class="form-control me-2" type="search" placeholder="I am looking for..." aria-label="Search">
+                            <button class="btn btn-outline-success" type="submit">Search</button>
+                        </div>
+                    </form>
+                </div>
+                <div class="f-nav-three">
+                    <div class="icons">
+                        <a href="{{ route('cart') }}" style="margin: 10px; text-decoration: none;">
+                            <img src="{{ asset('images/cart4.svg') }}" />
+                            <span>Cart</span>
+                        </a>
+                        <a href="" style="margin: 10px; text-decoration: none;" id="popup-trigger">
+                            <img src="{{ asset('images/person-check.svg') }}" />
+                            <span>Account</span>
+                        </a>
+                        <a href="" style="margin: 10px; text-decoration: none;">
+                            <img src="{{ asset('images/patch-question-fill.svg') }}" />
+                            <span>Help</span>
+                        </a>
+                    </div>
+                    <div class="popup" id="popup">
+                        <div class="popup-content">
+                            <h5 style="text-align: center;">Welcome Guest</h5>
+                            <p style="text-align: center;">Manage Cart, Orders & Wishlist</p>
+                            <hr />
+                            @if (Route::has('login'))
+                                <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
+                                    @auth
+                                        <a href="{{ url('/home') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Home</a>
+                                    @else
+                                        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"><button class="signin-btn">Sign In</button></a>
+                                        @if (Route::has('register'))
+                                            <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"><button class="signup-btn">Sign Up</button></a>
+                                        @endif
+                                    @endauth
+                                </div>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+            <!-- </div> -->
+            <!-- <div>
+                <nav class="f-nav-two" style="margin-top: 0;">
+                    <ul class="row-btn">
+                        <li><a href="{{ route('home') }}"><span>Home</span></a></li>
+                        <li><a href="{{ route('products') }}"><span>Products</span></a></li>
+                        <li><a href="{{ route('collections') }}"><span>Collections</span></a></li>
+                        <li><a href="{{ route('blog') }}"><span>Blog</span></a></li>
+                        <li><a href="{{ route('features') }}"><span>Features</span></a></li>
+                    </ul>
+                </nav>
+            </div> -->
         </div>
+<<<<<<< HEAD
         <hr style="margin-bottom: 0; margin-top: 0;" />
+=======
+        <!-- <hr style="margin-bottom: 0; margin-top: 0;"/> -->
+>>>>>>> 9fe34890 (nav fixes)
         <script>
             const popupTrigger = document.getElementById('popup-trigger');
             const popup = document.getElementById('popup');
@@ -104,5 +152,14 @@
                 var dropdownContent = document.querySelector('.dropdown-content');
                 dropdownContent.style.display === 'block' ? dropdownContent.style.display = 'none' : dropdownContent.style.display = 'block';
             }
+
+            function toggleNavs() {
+                var fNavTwo = document.querySelector('.f-nav-two');
+                var fNavThree = document.querySelector('.f-nav-three');
+                
+                // Toggle visibility of f-nav-two and f-nav-three
+                fNavTwo.classList.toggle('visible');
+                fNavThree.classList.toggle('visible');
+            }
         </script>
-    </div>
+    </header>
